a313c33e743e51774ffd8f311466fbfe0340dda3
Gregory.Johnson <3531op@gmail.com>
2021-08-08 16:03:03 -0400

    Backend is working for user function so far

--- routes/users/usersController.js
+++ routes/users/usersController.js
@@ -1,32 +1,38 @@
-const mongoErrorParser = require("../lib/mongoErrorParser")
-const User = require("../users/model/User")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcryptjs")
+const mongoErrorParser = require('../lib/mongoErrorParser');
+const User = require('../users/model/User');
+const bcrypt = require('bcryptjs');
 
+const signUp = async (req, res) => {
+	console.log(`======signUp ran======`);
+	try {
+		const salted = await bcrypt.genSalt(10);
+
+		const hashedPassword = await bcrypt.hash(req.body.password, salted);
+		console.log(`======hashedPassword======`);
+		console.log(hashedPassword);
+
+		const createdUser = new User({
+			email: req.body.email,
+			username: req.body.username,
+			password: hashedPassword,
+		});
+		// console.log(`======createdUser======`);
+		// console.log(createdUser);
+
+		const savedUser = await createdUser.save();
+		// console.log(`======savedUser======`);
+		// console.log(savedUser);
+
+		res.json({
+			data: savedUser,
+		});
+	} catch (error) {
+		res.status(500).json(mongoErrorParser(error));
+		console.log(error);
+		console.log(`======usersController signUp error ran======`);
+	}
+};
 
 module.exports = {
-  signUp = async (req, res) => {
-    try {
-      let salted = await bcrypt.genSalt(10);
-
-      let hashedPassword = await bcrypt.hash(req.body.password, salted)
-
-      const {username, email} = req.body;
-
-      let createdUser = new User({
-        email,
-        username, 
-        password: hashedPassword
-      })
-
-      let savedUser = await createdUser.savedUser()
-      res.json({
-        payload: savedUser
-      })
-
-    } catch (error) {
-      res.status(500).json({mongoErrorParser(error)})
-      console.log(`======usersController signUp error ran======`)
-    }
-  }
-}
+	signUp,
+};
--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const { signUp } = require('../users/usersController');
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 	res.send('respond with a resource');
 });
 
-router.post('/sign-up');
+router.post('/sign-up', signUp);
 
 module.exports = router;
