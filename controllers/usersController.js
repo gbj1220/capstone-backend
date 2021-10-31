72d58598171cfa3aaf45a7bfdce6272d95f3c43f
Gregory.Johnson <3531op@gmail.com>
2021-10-31 13:30:47 -0400

    added host string and api key string in env to fix connection issue

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,170 +7,343 @@ const axios = require('axios');
 require('dotenv').config();
 
 async function signUp(req, res) {
-	try {
-		//setting up salt to encrypt the password given to function
-		let salted = await bcrypt.genSalt(10);
+  try {
+    //setting up salt to encrypt the password given to function
+    let salted = await bcrypt.genSalt(10);
 
-		//hashing password to fully encrypt it
-		let hashedPassword = await bcrypt.hash(req.body.password, salted);
+    //hashing password to fully encrypt it
+    let hashedPassword = await bcrypt.hash(req.body.password, salted);
 
-		//creating a new user using the UserSchema model
-		let createdUser = new User({
-			email: req.body.email,
-			username: req.body.username,
-			password: hashedPassword,
-		});
+    //creating a new user using the UserSchema model
+    let createdUser = new User({
+      email: req.body.email,
+      username: req.body.username,
+      password: hashedPassword,
+    });
 
-		//waiting for saved user promise to return
-		let savedUser = await createdUser.save();
+    //waiting for saved user promise to return
+    let savedUser = await createdUser.save();
 
-		//returning the new saved user to the client side
-		res.json({
-			savedUser,
-		});
-	} catch (err) {
-		//if an err occurs, return a 400 error and return the err response to the client side
-		res.status(400).json({
-			err,
-		});
-	}
+    //returning the new saved user to the client side
+    res.json({
+      savedUser,
+    });
+  } catch (err) {
+    //if an err occurs, return a 400 error and return the err response to the client side
+    res.status(400).json({
+      err,
+    });
+  }
 }
 
 async function login(req, res) {
-	//create a var to store result of querying DB for username
-	try {
-		let foundUser = await User.findOne({ username: req.body.username });
+  //create a var to store result of querying DB for username
+  try {
+    let foundUser = await User.findOne({ username: req.body.username });
 
-		//throw an error if the username is not in the DB
-		if (!foundUser) {
-			throw 'Username not found';
-		}
+    //throw an error if the username is not in the DB
+    if (!foundUser) {
+      throw 'Username not found';
+    }
 
-		//using bcrypt to compare what the user typed in against what is in the DB
-		let comparedPassword = await bcrypt.compare(
-			req.body.password,
-			foundUser.password
-		);
+    //using bcrypt to compare what the user typed in against what is in the DB
+    let comparedPassword = await bcrypt.compare(
+      req.body.password,
+      foundUser.password
+    );
 
-		//if the password is incorrect throw an error message otherwise create a new jwtToken and assign it to the user
-		if (!comparedPassword) {
-			throw 'Password and Username must match';
-		} else {
-			let jwtToken = jwt.sign(
-				{
-					username: foundUser.username,
-				},
-				process.env.JWT_SECRET,
-				{ expiresIn: '3h' }
-			);
+    //if the password is incorrect throw an error message otherwise create a new jwtToken and assign it to the user
+    if (!comparedPassword) {
+      const RecipeSchema = require('../model/SavedRecipes');
+      const User = require('../model/User');
+      const jwt = require('jsonwebtoken');
+      const bcrypt = require('bcryptjs');
+      const axios = require('axios');
+      //requiring in dotenv so that I can access my .env file and change my port accordingly
+      require('dotenv').config();
 
-			//returning the jwtToken
+      async function signUp(req, res) {
+        try {
+          //setting up salt to encrypt the password given to function
+          let salted = await bcrypt.genSalt(10);
+
+          //hashing password to fully encrypt it
+          let hashedPassword = await bcrypt.hash(req.body.password, salted);
+
+          //creating a new user using the UserSchema model
+          let createdUser = new User({
+            email: req.body.email,
+            username: req.body.username,
+            password: hashedPassword,
+          });
+
+          //waiting for saved user promise to return
+          let savedUser = await createdUser.save();
+
+          //returning the new saved user to the client side
+          res.json({
+            savedUser,
+          });
+        } catch (err) {
+          //if an err occurs, return a 400 error and return the err response to the client side
+          res.status(400).json({
+            err,
+          });
+        }
+      }
+
+      async function login(req, res) {
+        //create a var to store result of querying DB for username
+        try {
+          let foundUser = await User.findOne({ username: req.body.username });
+
+          //throw an error if the username is not in the DB
+          if (!foundUser) {
+            throw 'Username not found';
+          }
+
+          //using bcrypt to compare what the user typed in against what is in the DB
+          let comparedPassword = await bcrypt.compare(
+            req.body.password,
+            foundUser.password
+          );
+
+          //if the password is incorrect throw an error message otherwise create a new jwtToken and assign it to the user
+          if (!comparedPassword) {
+            throw 'Password and Username must match';
+          } else {
+            let jwtToken = jwt.sign(
+              {
+                username: foundUser.username,
+              },
+              process.env.JWT_SECRET,
+              { expiresIn: '3h' }
+            );
+
+            //returning the jwtToken
+            res.json({ jwtToken })
+          }
+        } catch (err) {
+          console.log(`====== err ======`);
+          console.log(err);
+          res.json({ err }).status(400);
+        }
+      }
+
+      //creating an async function in order to call the recipe api
+      async function getRecipeData(req, res) {
+        console.log(`usrInput: ${req.body.usrInput}`)
+        try {
+          //destructuring req object
+          const { usrInput } = req.body;
+
+          //creating a var the save the api call response to
+          let response = await axios.get(
+            'https://edamam-recipe-search.p.rapidapi.com/search',
+            {
+              params: { q: usrInput },
+              headers: {
+                'x-rapidapi-key': process.env.FOOD_API_KEY,
+                'x-rapidapi-host': process.env.FOOD_API_HOST,
+              },
+            }
+          );
+          const data = response.data
+          //returning the data
+          res.json({ data });
+        } catch (err) {
+          //otherwise send back err object to client side
+          res.status(400).json({ err });
+        }
+      }
+
+      //function which adds a selected recipe to the users DB
+      async function saveRecipe(req, res) {
+        //destructuring req object
+        try {
+          const { label, image, recipeLink } = req.body;
+
+          //using the RecipeSchema to save a recipe to users DB
+          let recipe = await new RecipeSchema({
+            label,
+            image,
+            recipeLink,
+          });
+
+          let newSavedRecipe = await recipe.save();
+
+          let token = req.headers.authorization.slice(7);
+          console.log(`====== token ======`);
+          console.log(token);
+
+          let decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+          console.log(`====== decodedToken ======`);
+          console.log(decodedToken);
+
+          let targetUser = await User.findOne({
+            username: decodedToken.username,
+          });
+
+          console.log(`====== target user ======`);
+          console.log(targetUser);
+          targetUser.recipes.push(newSavedRecipe._id);
+          console.log(`====== new saved recipe ======`);
+          console.log(newSavedRecipe);
+
+          await targetUser.save();
+
+          res.json({
+            newSavedRecipe,
+          });
+        } catch (err) {
+          res.status(400).json({
+            err,
+          });
+        }
+      }
+
+      //maybe do an onClick handler for the view saved recipes button || useEffect on UsersRecipes.js to always pull new data when component is loaded.
+
+      async function getRecipes(req, res) {
+        try {
+          let jwtToken = req.headers.authorization.slice(7);
+          let decodedToken = jwt.verify(jwtToken, process.env.JWT_SECRET);
+          let payload = await User.findOne({ username: decodedToken.username })
+            .populate({
+              path: 'recipes',
+              model: 'savedRecipe',
+              select: '-__v',
+            })
+            .select('-email -username -password');
+          res.json(payload);
+        } catch (err) {
+          res.status(400).json({
+            err,
+          });
+        }
+      }
+
+      module.exports = {
+        getRecipeData,
+        getRecipes,
+        saveRecipe,
+        login,
+        signUp,
+      };
+    } else {
+      let jwtToken = jwt.sign(
+        {
+          username: foundUser.username,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '3h' }
+      );
+
+      //returning the jwtToken
       res.json({ jwtToken })
-		}
-	} catch (err) {
-		console.log(`====== err ======`);
-		console.log(err);
-		res.json({ err }).status(400);
-	}
+    }
+  } catch (err) {
+    console.log(`====== err ======`);
+    console.log(err);
+    res.json({ err }).status(400);
+  }
 }
 
 //creating an async function in order to call the recipe api
 async function getRecipeData(req, res) {
-  console.log(req.body.usrInput)
-	try {
-		//destructuring req object
-		const { usrInput } = req.body;
+  console.log(`usrInput: ${req.body.usrInput}`)
+  try {
+    //destructuring req object
+    const { usrInput } = req.body;
 
-		//creating a var the save the api call response to
-		let response = await axios.get(
-			'https://edamam-recipe-search.p.rapidapi.com/search',
-			{
-				params: { q: usrInput },
-				headers: {
-					'x-rapidapi-key': process.env.FOOD_API_KEY,
-					'x-rapidapi-host': process.env.FOOD_API_HOST,
-				},
-			}
-		);
-      const data = response.data
-		//returning the data
-		res.json({ data });
-	} catch (err) {
-		//otherwise send back err object to client side
-		res.status(400).json({ err });
-	}
+    //creating a var the save the api call response to
+    let response = await axios.get(
+      'https://edamam-recipe-search.p.rapidapi.com/search',
+      {
+        params: { q: usrInput },
+        headers: {
+          'x-rapidapi-host': process.env.FOOD_API_HOST,
+          'x-rapidapi-key': process.env.FOOD_API_KEY,
+        },
+      }
+    );
+    //returning the data
+    const data = response.data
+    res.json({ data });
+  } catch (err) {
+    //otherwise send back err object to client side
+    res.status(400).json({ err });
+  }
 }
 
 //function which adds a selected recipe to the users DB
 async function saveRecipe(req, res) {
-	//destructuring req object
-	try {
-		const { label, image, recipeLink } = req.body;
+  //destructuring req object
+  try {
+    const { label, image, recipeLink } = req.body;
 
-		//using the RecipeSchema to save a recipe to users DB
-		let recipe = await new RecipeSchema({
-			label,
-			image,
-			recipeLink,
-		});
+    //using the RecipeSchema to save a recipe to users DB
+    let recipe = await new RecipeSchema({
+      label,
+      image,
+      recipeLink,
+    });
 
-		let newSavedRecipe = await recipe.save();
+    let newSavedRecipe = await recipe.save();
 
-		let token = req.headers.authorization.slice(7);
-		console.log(`====== token ======`);
-		console.log(token);
+    let token = req.headers.authorization.slice(7);
+    console.log(`====== token ======`);
+    console.log(token);
 
-		let decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-		console.log(`====== decodedToken ======`);
-		console.log(decodedToken);
+    let decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(`====== decodedToken ======`);
+    console.log(decodedToken);
 
-		let targetUser = await User.findOne({
-			username: decodedToken.username,
-		});
+    let targetUser = await User.findOne({
+      username: decodedToken.username,
+    });
 
-		console.log(`====== target user ======`);
-		console.log(targetUser);
-		targetUser.recipes.push(newSavedRecipe._id);
-		console.log(`====== new saved recipe ======`);
-		console.log(newSavedRecipe);
+    console.log(`====== target user ======`);
+    console.log(targetUser);
+    targetUser.recipes.push(newSavedRecipe._id);
+    console.log(`====== new saved recipe ======`);
+    console.log(newSavedRecipe);
 
-		await targetUser.save();
+    await targetUser.save();
 
-		res.json({
-			newSavedRecipe,
-		});
-	} catch (err) {
-		res.status(400).json({
-			err,
-		});
-	}
+    res.json({
+      newSavedRecipe,
+    });
+  } catch (err) {
+    res.status(400).json({
+      err,
+    });
+  }
 }
 
-//maybe do an onClick handler for the view saved recipes button || useEffect on UsersRecipes.js to always pull new data when component is loaded.
-
 async function getRecipes(req, res) {
-	try {
-		let jwtToken = req.headers.authorization.slice(7);
-		let decodedToken = jwt.verify(jwtToken, process.env.JWT_SECRET);
-		let payload = await User.findOne({ username: decodedToken.username })
-			.populate({
-				path: 'recipes',
-				model: 'savedRecipe',
-				select: '-__v',
-			})
-			.select('-email -username -password');
-		res.json(payload);
-	} catch (err) {
-		res.status(400).json({
-			err,
-		});
-	}
+  try {
+    let jwtToken = req.headers.authorization.slice(7);
+    let decodedToken = jwt.verify(jwtToken, process.env.JWT_SECRET);
+    let payload = await User.findOne({ username: decodedToken.username })
+      .populate({
+        path: 'recipes',
+        model: 'savedRecipe',
+        select: '-__v',
+      })
+      .select('-email -username -password');
+    res.json(payload);
+  } catch (err) {
+    res.status(400).json({
+      err,
+    });
+  }
 }
 
 module.exports = {
-	getRecipeData,
-	getRecipes,
-	saveRecipe,
-	login,
-	signUp,
+  getRecipeData,
+  getRecipes,
+  saveRecipe,
+  login,
+  signUp,
 };
